6b130a2e8fe2d3c9d428603887586d848349b319
tarumam <tarumamiranda@gmail.com>
2018-01-30 08:47:11 -0200

    Contador

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,20 @@ module.exports = {
         port: 8080,
         contentBase: './public'
     },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        alias:{
+            modules: __dirname + '/node_modules'
+        }
+    },
     module:{
             loaders:[{
-                test: /.jsx?$/,
+                test: /.js[x]?$/,
                 loader:'babel-loader',
                 exclude: /node_modules/,
                 query:{
-                    presets:['es2015','react']
+                    presets:['es2015','react'],
+                    plugins: ['transform-object-rest-spread']
                 }
             }]
     }
